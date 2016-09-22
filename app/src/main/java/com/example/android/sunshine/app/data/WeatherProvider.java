9bc29347dfe3f9a761384a48ffca147bd1e1c983
Jamie Olsen <jwolsen@mmm.com>
2016-09-22 12:56:59 -0600

    working on uri matcher

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherProvider.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherProvider.java
@@ -117,11 +117,11 @@ public class WeatherProvider extends ContentProvider {
     static UriMatcher buildUriMatcher() {
         // 1) The code passed into the constructor represents the code to return for the root
         // URI.  It's common to use NO_MATCH as the code for this case. Add the constructor below.
-
+        UriMatcher mUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
         // 2) Use the addURI function to match each of the types.  Use the constants from
         // WeatherContract to help define the types to the UriMatcher.
-
+        mUriMatcher.addURI(WEATHER);
 
         // 3) Return the new matcher!
         return null;
